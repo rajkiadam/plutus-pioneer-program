f1d17909174cb90a15b0e879622aa402cad61036
midBaller08 <rajkiadam@gmail.com>
2023-04-28 17:48:43 +0000

    Week08 Homework

--- code/Week08/homework/Homework.hs
+++ code/Week08/homework/Homework.hs
@@ -15,12 +15,15 @@ module Homework
     ) where
 
 import           Plutus.V2.Ledger.Api (Address, BuiltinData, PubKeyHash,
-                                       ScriptContext, StakeValidator,
-                                       mkStakeValidatorScript)
+                                       ScriptContext (scriptContextPurpose, scriptContextTxInfo), StakeValidator,
+                                       mkStakeValidatorScript, ScriptPurpose (Certifying, Rewarding), TxOut (txOutAddress, txOutValue), TxInfo (txInfoWdrl, txInfoOutputs), StakingCredential)
 import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (..), ($), (.))
-import           Prelude              (IO, String, undefined)
+import qualified PlutusTx.AssocMap     as PlutusTx
+import           PlutusTx.Prelude     (Bool (..), ($), (.), traceIfFalse, traceError)
+import           Prelude              (IO, String, undefined, Num ((*), (+)), Integer, Maybe (Just, Nothing), Ord ((>=)), Foldable (foldl), Eq ((==)), otherwise, (&&))
 import           Utilities            (wrapStakeValidator)
+import Plutus.V2.Ledger.Contexts (txSignedBy)
+import Plutus.V1.Ledger.Value (valueOf, adaSymbol, adaToken)
 
 -- | A staking validator with two parameters, a pubkey hash and an address. The validator
 --   should work as follows:
@@ -28,7 +31,27 @@ import           Utilities            (wrapStakeValidator)
 --   2.) The given address needs to receive at least half of all withdrawn rewards.
 {-# INLINABLE mkStakeValidator' #-}
 mkStakeValidator' :: PubKeyHash -> Address -> () -> ScriptContext -> Bool
-mkStakeValidator' _pkh _addr () _ctx = undefined
+mkStakeValidator' _pkh _addr () _ctx = traceIfFalse "missing signature" $ txSignedBy info _pkh && 
+    case scriptContextPurpose _ctx of
+        Certifying _   -> True        
+        Rewarding cred -> traceIfFalse "insufficient reward sharing" $ 2 * paidToAddress >= amount cred
+        _              -> False
+    where
+        info :: TxInfo
+        info = scriptContextTxInfo _ctx
+
+        amount :: StakingCredential -> Integer
+        amount cred = case PlutusTx.lookup cred $ txInfoWdrl info of
+            Just amt -> amt
+            Nothing  -> traceError "withdrawal not found"
+
+        paidToAddress :: Integer
+        paidToAddress = foldl f 0 $ txInfoOutputs info
+            where
+                f :: Integer -> TxOut -> Integer
+                f n o
+                    | txOutAddress o == _addr = n + valueOf (txOutValue o) adaSymbol adaToken
+                    | otherwise              = n
 
 {-# INLINABLE mkWrappedStakeValidator' #-}
 mkWrappedStakeValidator' :: PubKeyHash -> Address -> BuiltinData -> BuiltinData -> ()
